29d03d93d82dcc80ca9975f5f53b349b0420fc1e
roman <romanitalian@yandex.ru>
2015-07-28 21:21:51 +0300

    add new test files

---
+++ made_in_phpstorm.php
@@ -0,0 +1,8 @@
+Привет, Мир!
+
+
+Settings -> File encoding:
+IDE encoding: UTF-8
+Project encoding: windows-1251
+
+When I make new file: it makes it in windows-1251
---
+++ made_in_phpstorm2.php
@@ -0,0 +1,7 @@
+Привет, Мир!
+
+Settings -> File encoding:
+IDE encoding: win1251
+Project encoding: utf8
+
+When I make new file: it makes it in utf8
